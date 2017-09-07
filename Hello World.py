3edb9b54b18de44fe062922f7d5a7246d22dd229
OscarBohlin <31740264+OscarBohlin@users.noreply.github.com>
2017-09-07 18:51:07 +0200

    Lekt med git
    Nurå

---
+++ Hello World.py
@@ -0,0 +1,52 @@
+print("Hellow World nu på Git nu också ny version")
+
+a = 10
+b = 20
+
+c = a * b
+
+#print("Svaret är ", c, "så det så")
+
+#print ("Vad heter du?")
+#svar = input()
+#print("Hej", svar, "hur mår du?")
+
+#if svar == "Oscar" :
+#    print ("Nice XDXD")
+#else: print (" Wrong name bro")
+##
+##
+##def addera(a,b):
+##    return a+b
+####
+##
+##def best_name():
+##    return "Oscar"
+##
+##
+##
+##
+##
+##print("The best name is", best_name())
+##
+##
+##print(addera(4,6))
+
+import pycurl
+
+
+print("Go'morron")
+svar = input()
+if svar == ("same"):
+    while True:
+        print("XD")
+else:
+    print("Hur mår du?")
+    x = input()
+    if x == "bra":
+        while True:
+           print("XD")
+        
+    
+    
+##    
